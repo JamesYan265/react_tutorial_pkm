f1bcf5337362d91b47e7868da426a89c867893dc
JamesYan265 <JamesYan265@protonmail.com>
2022-12-02 22:42:25 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pkmData, setPkmData] = useState([]);
+  const [nextURL, setNextURL] = useState("");
+  const [prevURL, setPrevURL] = useState("");
+
+  // Promise.all 係[等*所有map, array]實現時回傳一個Promise物件
+  const PokemonDetail = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPkmData(_pokemonData);
+  };
+
+  useEffect(() => {
+    const fetchPokemonData = async() => {
+      //取得資料
+      let res = await getAllPokemon(initialURL);
+      //各隻Pokemon資料取得
+      //取得再詳細的Data 
+      PokemonDetail(res.results);
+      setNextURL(res.next);
+      setPrevURL(res.previous);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  },[])
+
+  const handlePrevPage = async() => {
+    if(!prevURL) return;
+
+    setLoading(true);
+    let data = await getAllPokemon(prevURL);
+    await PokemonDetail(data.results);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
+  const handleNextPage = async() => {
+    setLoading(true);
+    let data = await getAllPokemon(nextURL);
+    await PokemonDetail(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <div className='App'>
+        {loading ? (
+          <h1>讀取中</h1>
+        ) : (
+          <>
+            <div className='pokemonCardContariner'>
+              {pkmData.map((pokemon, index) => {
+                return <Card key={index} pokemon={pokemon}/>
+              })}
+            </div>
+            <div className='btn'>
+              <button onClick={handlePrevPage}>前一頁</button>
+              <button onClick={handleNextPage}>後一頁</button>
+            </div>          
+          </>
+
+        )}
+      </div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>Pokemon 圖鑑</nav>
+  )
+}
+
+export default Navbar
